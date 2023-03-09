022949fda54e75328dcdd0504162bf5929d387f6
Jesse Brink <jbrink0925@gmail.com>
2023-03-08 23:40:24 -0500

    add nonblocking tcp server

---
+++ nonblocking_tcp_server.py
@@ -0,0 +1,129 @@
+import selectors
+import socket
+import types
+
+from typing import Any, Final, List, NamedTuple, Tuple
+
+
+class Address(NamedTuple):
+    ip_address: str
+    port: int
+
+
+# Constants
+SERVER_ADDRESS: Final[str] = "127.0.0.1"
+SERVER_PORT: Final[int] = 27685
+DEFAULT_SERVER_ADDRESS: Address = Address(SERVER_ADDRESS, SERVER_PORT)
+MAXIMUM_TRANSMISSIBLE_UNIT: int = 1500
+
+
+class TCPServer:
+    def __init__(
+            self, 
+            server_address: Address = DEFAULT_SERVER_ADDRESS
+    ) -> None:
+        self._server_address: Address = server_address
+        self._sock: socket.socket = self._create_socket()
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._selector: selectors.DefaultSelector = selectors.DefaultSelector()
+
+    def _create_socket(self) -> socket.socket:
+        return socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        )
+
+    def _bind_and_listen(self) -> None:
+        self._sock.bind(
+            (self._server_address.ip_address,
+             self._server_address.port
+        ))
+        self._sock.listen()
+        self._sock.setblocking(0)
+        self._selector.register(self._sock, selectors.EVENT_READ, data=None)
+
+    def accept_connections_and_echo(self):
+        self._bind_and_listen()
+
+        # Event Loop
+        while True:
+            """
+            "events" is a list of tuples.
+                - events[0] is a SelectorKey object, which has the following attributes:
+                    - key.fileobj - a TCP socket to read from / write to, and close
+                    - key.fd - a file descriptor (an integer)
+                    - key.data - pretty much anything you want. In this example, it is
+                      a SimpleNameSpace containing:
+                        - remote address
+                        - data (outbound data buffer, inbound data buffer)
+            
+            We set "setblocking" to False here, just as we did with the listening socket above.
+            """
+            events: List[Tuple[Any, Any]]= self._selector.select()
+            for key, mask in events:
+                if not key.data:
+                    remote_sock, remote_address = self._sock.accept()
+                    print(f"Accepted connection from {remote_address}")
+                    remote_sock.setblocking(0)
+                    data: types.SimpleNamespace = types.SimpleNamespace(
+                        addr=remote_address,
+                        inbound_data = b"",
+                        outbound_data = b""
+                    )
+                    """
+                    By default, a socket registered in "select" has data=None
+                    If data is None, we know it is a new connection that is not yet registered
+                    We set its data which, for us, is the remote address and two byte buffers
+                    for inbound and outbound.
+                    Finally, we register it with select.
+
+                    NOTE: register takes a new connection and registers it with select. Its args
+                    are:
+                        - the actual remote socket
+                        - a bitmask to check to see if the socket is waiting to read, write, or both
+                        - the new data object, created above
+                    """
+                    self._selector.register(remote_sock, selectors.EVENT_READ | selectors.EVENT_WRITE, data)
+                else:
+                    # Get the actual socket and the data from the SelectorKEy object.
+                    sock: socket.socket = key.fileobj
+                    data: bytes = key.data
+
+                    """
+                    If the remote socket is waiting to be read:
+                        - call recv on the remote socket to get data from it
+                        - check to see if it is not None
+                            - if not, add the new data to the data object's outbound_data buffer
+                        
+                        - otherwise, it's already been read and we can close the connection
+                    """
+                    if mask & selectors.EVENT_READ:
+                        recv_data: bytes = remote_sock.recv(MAXIMUM_TRANSMISSIBLE_UNIT)
+                        if recv_data:
+                            data.outbound_data += recv_data
+                        else:
+                            print(f"Closing connection to {data.addr}")
+                            self._selector.unregister(sock)
+                            sock.close()
+
+                    """
+                    If the remote socket is waiting to be written to:
+                        - check to see if there is outbound data to send
+                        - if so, loop over the data and send it back
+                        - finally, unregister the socket with the selector and then close the socket.
+                    """
+                    if mask & selectors.EVENT_WRITE:
+                        if data.outbound_data:
+                            print(f"Echoing {data.outbound_data} to {data.addr}")
+                            while data.outbound_data:
+                                sent: int = sock.send(data.outbound_data)
+                                data.outbound_data = data.outbound_data[sent:]
+
+                            self._selector.unregister(sock)
+                            if sent and not data.outbound_data:
+                                sock.close()
+
+
+if __name__ == "__main__":
+    tcp_server: TCPServer = TCPServer()
+    tcp_server.accept_connections_and_echo()
